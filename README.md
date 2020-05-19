2bbff6927a2f965a2644cb474d82c7baed95b1d8
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-19 13:52:49 -0400

    finalizing

---
+++ README.md
@@ -0,0 +1,11 @@
+# Non-Live NYU Dataset
+
+Courses data acquired by Schedge. To get the most recent data, run 
+
+`python3 downloader.py --term --year`
+
+Term: fa, sp, su, ja
+
+# Goals
+
+Hopefully, this can be a good resource for NYU student to use these dataset to carry out cool projects.
--- downloader.py
+++ downloader.py
@@ -1,16 +1,18 @@
 import requests as re
 import json
+import sys
 
 ROOT_URL = "https://schedge.a1liu.com/"
 SUBJECT_URL = ROOT_URL + "subjects"
-sem = "fa"
-year = "2020"
 sems = ["fa", "su", "sp", "ja"]
-
 res = re.get(SUBJECT_URL)
 contents = json.loads(res.content)
-
 data = []
+
+sem = sys.argv[1]
+year = sys.argv[2]
+if sem not in sems:
+    raise ValueError("Invalid semester for " + sem)
 
 #handle error later
 for subject in contents:
@@ -18,10 +20,12 @@ for subject in contents:
         print("Retrieving course data for " + subjectCode + "-" + subject)
         result = re.get(ROOT_URL + year + "/" + sem + "/" + subject + "/" + subjectCode)
         courses = json.loads(result.content)
-        data.append(courses)
+        if not courses:
+            print("No available data for " + subjectCode + "-" + subject)
+        else:
+            data.append(courses)
 
-with open(year + sem + ".json", "w") as f:
+with open("data/" + year + sem + ".json", "w") as f:
     for course in data:
-        if not not course: 
-            f.write("%s\n" % course)
+        f.write("%s\n" % course)
 
